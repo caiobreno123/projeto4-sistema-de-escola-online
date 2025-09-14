649bacbadb4c82b64d7b3c91e58c21f9e70fcadc
caiobreno123 <caiobrenopiresduarte@gmail.com>
2025-09-14 08:33:58 -0300

    Update Turma.js

--- class/Turma.js
+++ class/Turma.js
@@ -5,15 +5,14 @@ class Turma {
     this.disciplinas = [];
   }
 
-  // Método para adicionar alunos à turma
   adicionarAluno(aluno) {
     this.alunos.push(aluno);
   }
 
-  // Método para adicionar disciplinas à turma
   adicionarDisciplina(disciplina) {
     this.disciplinas.push(disciplina);
   }
 }
 
 module.exports = Turma;
+
