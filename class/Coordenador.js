337a0466f21784e344752f5f1e4efc7df5aec6bd
caiobreno123 <caiobrenopiresduarte@gmail.com>
2025-09-14 08:32:45 -0300

    Update Coordenador.js

--- class/Coordenador.js
+++ class/Coordenador.js
@@ -5,10 +5,10 @@ class Coordenador extends Usuario {
     super(nome, idade);
   }
 
-  // Sobrescreve o método acessoPainel
   acessoPainel() {
     return "Painel do Coordenador: administração escolar";
   }
 }
 
 module.exports = Coordenador;
+
