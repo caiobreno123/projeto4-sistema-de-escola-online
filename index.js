ca988f0c2d20a6420f95de84a5bc1f69cd01d358
caiobreno123 <caiobrenopiresduarte@gmail.com>
2025-09-14 08:36:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,30 +5,25 @@ const Disciplina = require("./class/Disciplina");
 const Turma = require("./class/Turma");
 const Nota = require("./class/Nota");
 
-// Criando usuários
 const aluno = new Aluno("Maria", 16);
 const professor = new Professor("João", 35);
 const coordenador = new Coordenador("Ana", 40);
 
-// Testando acesso ao painel
-console.log(aluno.acessoPainel()); // "Painel do Aluno: consultar notas e disciplinas"
-console.log(professor.acessoPainel()); // "Painel do Professor: gerenciar notas e turmas"
-console.log(coordenador.acessoPainel()); // "Painel do Coordenador: administração escolar"
+console.log(aluno.acessoPainel());
+console.log(professor.acessoPainel());
+console.log(coordenador.acessoPainel());
 
-// Criando disciplinas e turma
 const matematica = new Disciplina("Matemática", 80);
 const turmaA = new Turma("1ºA");
 
-// Adicionando alunos e disciplinas à turma
 turmaA.adicionarDisciplina(matematica);
 turmaA.adicionarAluno(aluno);
 
-// Verificando as disciplinas e alunos da turma
-console.log(turmaA.disciplinas); // [ Disciplina { nome: 'Matemática', cargaHoraria: 80 } ]
-console.log(turmaA.alunos); // [ Aluno { nome: 'Maria', idade: 16 } ]
+console.log(turmaA.disciplinas);
+console.log(turmaA.alunos);
 
-// Criando e atribuindo uma nota ao aluno
 const nota1 = new Nota(8.5, matematica);
 aluno.adicionarNota(nota1);
 
-console.log(aluno.notas); // [ Nota { valor: 8.5, disciplina: Disciplina { nome: 'Matemática', cargaHoraria: 80 } } ]
+console.log(aluno.notas);
+
